aff79c5c7d62b649960a187a4568be7f7feb97b3
João Henrique <joaohen.t8@gmail.com>
2024-10-21 16:43:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const hospital = ['joao','gustavo','guilherme','thierry']
-while(hospital){
-let choice = prompt(`os seus pacientes são:\n ${hospital}\n\n1-CONSULTAR PACINTE\n2-NOVO PACIENTE\n3-SAIR `)
-
-if(choice = 1){
-   hospital.shift()
-
-}
-else if (choice = 2){
- let nova = prompt('qual o nome do novo paciente?')
- hospital.push(nova)
- 
-}
+let hospital = ['joao','gustavo','guilherme','thierry']
+let pick = prompt(`os seus pacientes são:\n ${hospital}\n\n1-CONSULTAR PACIENTE\n2-NOVO PACIENTE\n3-SAIR `)
+while(pick !=3) {
+    let pick = prompt(`os seus pacientes são:\n ${hospital}\n\n1-CONSULTAR PACIENTE\n2-NOVO PACIENTE\n3-SAIR `)
+    if(pick==1){
+        hospital.shift()
+    }
+    else if(pick == 2){
+        let name =prompt('qual o nome do paciente?')
+        hospital.push(name)
+    }
+    else{ pick=3
+        alert('programa encerrado')
+        break
+    }
+ }
+    
