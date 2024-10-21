ec692fed10cc15d291c4b305211b26743e17e28e
João Henrique <joaohen.t8@gmail.com>
2024-10-21 16:53:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let hospital = ['joao','gustavo','guilherme','thierry']
-let pick = prompt(`os seus pacientes são:\n ${hospital}\n\n1-CONSULTAR PACIENTE\n2-NOVO PACIENTE\n3-SAIR `)
-while(pick !=3) {
+let pick2 = prompt(`Hospital aberto,\nEscreva 1 para iniciar`)
+while(pick2 !=3) {
     let pick = prompt(`os seus pacientes são:\n ${hospital}\n\n1-CONSULTAR PACIENTE\n2-NOVO PACIENTE\n3-SAIR `)
     if(pick==1){
         hospital.shift()
